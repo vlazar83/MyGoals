0f20b389a14fec6a16dc5df8024ab7d2a79cbf3f
admin <admin@admins-MacBook-Pro.local>
2020-05-13 15:09:02 +0200

    Camera permission handling

--- app/src/main/java/com/vlazar83/mygoals/FinalizeNewCardActivity.java
+++ app/src/main/java/com/vlazar83/mygoals/FinalizeNewCardActivity.java
@@ -2,9 +2,13 @@ package com.vlazar83.mygoals;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -17,6 +21,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.gson.Gson;
@@ -34,6 +39,7 @@ public class FinalizeNewCardActivity extends AppCompatActivity {
 
     private CreatedCardSet createdCardSet;
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int MY_PERMISSIONS_REQUEST_CAMERA = 10;
     private ImageView takenPicture;
     private String currentPhotoPath;
     private Boolean pictureTakenFromCamera;
@@ -61,27 +67,45 @@ public class FinalizeNewCardActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if(CreateNewCardActivity.cardToPassOnForCreation != null && !pictureTakenFromCamera){
+                if(firstDetails.getText().toString() != null && !firstDetails.getText().toString().isEmpty() && secondDetails.getText().toString() != null && !secondDetails.getText().toString().isEmpty()){
+                    if(CreateNewCardActivity.cardToPassOnForCreation != null && !pictureTakenFromCamera){
 
-                    CardShape selectedSampleCard = CreateNewCardActivity.cardToPassOnForCreation;
-                    CardShape newCard = createTheNewCard(selectedSampleCard.getCardClass(), firstDetails.getText().toString(), secondDetails.getText().toString(), selectedSampleCard.getCardUrl());
-                    createdCardSet.addCard(newCard);
-                    saveCreatedCardsToSharedPreferences();
-                } else if (CreateNewCardActivity.cardToPassOnForCreation != null && pictureTakenFromCamera){
-                    CardShape selectedSampleCard = CreateNewCardActivity.cardToPassOnForCreation;
-                    CardShape newCard = createTheNewCard(selectedSampleCard.getCardClass(), firstDetails.getText().toString(), secondDetails.getText().toString(), currentPhotoPath);
-                    createdCardSet.addCard(newCard);
-                    saveCreatedCardsToSharedPreferences();
+                        CardShape selectedSampleCard = CreateNewCardActivity.cardToPassOnForCreation;
+                        CardShape newCard = createTheNewCard(selectedSampleCard.getCardClass(), firstDetails.getText().toString(), secondDetails.getText().toString(), selectedSampleCard.getCardUrl());
+                        createdCardSet.addCard(newCard);
+                        saveCreatedCardsToSharedPreferences();
+                    } else if (CreateNewCardActivity.cardToPassOnForCreation != null && pictureTakenFromCamera){
+                        CardShape selectedSampleCard = CreateNewCardActivity.cardToPassOnForCreation;
+                        CardShape newCard = createTheNewCard(selectedSampleCard.getCardClass(), firstDetails.getText().toString(), secondDetails.getText().toString(), currentPhotoPath);
+                        createdCardSet.addCard(newCard);
+                        saveCreatedCardsToSharedPreferences();
+                    }
+
+                    backToNewCardCreation();
+                } else {
+                    Toast.makeText(FinalizeNewCardActivity.this, getString(R.string.FinalizeNewCard_Toast_Message),
+                            Toast.LENGTH_LONG).show();
                 }
 
-                backToNewCardCreation();
             }
         });
 
         takePictureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dispatchTakePictureIntent();
+
+                if (ContextCompat.checkSelfPermission(FinalizeNewCardActivity.this,
+                        Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    ActivityCompat.requestPermissions(FinalizeNewCardActivity.this,
+                            new String[]{Manifest.permission.CAMERA},
+                            MY_PERMISSIONS_REQUEST_CAMERA);
+                } else {
+                    // Permission has already been granted
+                    dispatchTakePictureIntent();
+                }
+
             }
         });
 
@@ -108,6 +132,29 @@ public class FinalizeNewCardActivity extends AppCompatActivity {
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
             }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    dispatchTakePictureIntent();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
         }
     }
 
