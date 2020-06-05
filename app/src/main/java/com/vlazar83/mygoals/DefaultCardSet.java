5660dae37f6216795dd835954c82b48bff7d1dfe
admin <admin@admins-MacBook-Pro.local>
2020-06-05 13:11:07 +0200

    graphics for defaultCardSet added

--- app/src/main/java/com/vlazar83/mygoals/DefaultCardSet.java
+++ app/src/main/java/com/vlazar83/mygoals/DefaultCardSet.java
@@ -33,12 +33,20 @@ public class DefaultCardSet {
 
     private void fillWithDefaultSet(){
         CardFactory cardFactory = new CardFactory();
-        addCard(cardFactory.getCardShape("RedCard", "Istentisztelet", "Vasarnaponkent", getUriToResource(MyGoals.getAppContext(),R.drawable.istentisztelet_szines_600_800).toString()));
-        addCard(cardFactory.getCardShape("BlueCard", "Olvasas", "Regenyeket", getUriToResource(MyGoals.getAppContext(),R.drawable.olvasas_szines_600_800).toString()));
-        addCard(cardFactory.getCardShape("GreenCard", "Tarsasjatek", "Monopoly", getUriToResource(MyGoals.getAppContext(),R.drawable.tarsasjatek_szines_800_600).toString()));
-        addCard(cardFactory.getCardShape("LightGreenCard", "LightGreenCard example", "Kyoto", "https://source.unsplash.com/buF62ewDLcQ/600x800"));
-        addCard(cardFactory.getCardShape("RedCard", "put the trash out4", "New York", "https://source.unsplash.com/THozNzxEP3g/600x800"));
-        addCard(cardFactory.getCardShape("GreenCard", "put the trash out5", "New York", "https://source.unsplash.com/USrZRcRS2Lw/600x800"));
+        addCard(cardFactory.getCardShape("LightGreenCard", "Self-Time", "reading a book", getUriToResource(MyGoals.getAppContext(),R.drawable.green_card_introvert_1).toString()));
+        addCard(cardFactory.getCardShape("GreenCard", "Self-Time", "board game night with the family", getUriToResource(MyGoals.getAppContext(),R.drawable.green_card_extrovert_1).toString()));
+        addCard(cardFactory.getCardShape("LightGreenCard", "Self-Time", "pray", getUriToResource(MyGoals.getAppContext(),R.drawable.green_card_introvert_2).toString()));
+        addCard(cardFactory.getCardShape("GreenCard", "Self-Time", "football with friends", getUriToResource(MyGoals.getAppContext(),R.drawable.green_card_extrovert_2).toString()));
+        addCard(cardFactory.getCardShape("GreenCard", "Self-Time", "other activities for yourself", getUriToResource(MyGoals.getAppContext(),R.drawable.green_card_generic).toString()));
+
+        addCard(cardFactory.getCardShape("BlueCard", "Family", "visit your family", getUriToResource(MyGoals.getAppContext(),R.drawable.blue_card_1).toString()));
+        addCard(cardFactory.getCardShape("BlueCard", "Family", "family trip in the woods", getUriToResource(MyGoals.getAppContext(),R.drawable.blue_card_2).toString()));
+        addCard(cardFactory.getCardShape("BlueCard", "Family", "other activities with your loved ones", getUriToResource(MyGoals.getAppContext(),R.drawable.blue_card_generic).toString()));
+
+        addCard(cardFactory.getCardShape("RedCard", "Work", "worships on Sundays", getUriToResource(MyGoals.getAppContext(),R.drawable.red_card_1).toString()));
+        addCard(cardFactory.getCardShape("RedCard", "Work", "teaching about God's love", getUriToResource(MyGoals.getAppContext(),R.drawable.red_card_2).toString()));
+        addCard(cardFactory.getCardShape("RedCard", "Work", "preparations...", getUriToResource(MyGoals.getAppContext(),R.drawable.red_card_3).toString()));
+        addCard(cardFactory.getCardShape("RedCard", "Work", "other work related activities", getUriToResource(MyGoals.getAppContext(),R.drawable.red_card_generic).toString()));
     }
 
     public void addCard(CardShape cardShape){
